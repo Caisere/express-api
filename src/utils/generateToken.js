a8c36ae06681de2861bf8a1c59472da1197b92a7
Omoshola <heylehgbedey@gmail.com>
2025-12-14 02:25:09 -0800

    fix: validate JWT_SECRET in token generation
    - Added a check to ensure JWT_SECRET is defined before generating a token.
    - Throws an error if JWT_SECRET is not valid, improving error handling in the token generation process.

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,6 +2,11 @@ import jwt from 'jsonwebtoken'
 
 export const generateToken = (userId, res) => {
     const payload = {id: userId}
+
+    if(!process.env.JWT_SECRET) {
+        throw new Error('JWT_SECRET not defined or valid')
+    }
+
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN || "7d",
 
@@ -13,6 +18,6 @@ export const generateToken = (userId, res) => {
         sameSite: 'strict', 
         maxAge: (1000 * 60 * 60 * 24) * 7 // expires in 7days
     })
-    
+
     return token
 }
